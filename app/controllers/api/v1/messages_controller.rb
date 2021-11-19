e5f8836b6cfa17b48b7dd7f44c65976a9dd16bb8
petudeveloper <davidalvarezmazzo@gmail.com>
2021-11-19 16:24:07 -0500

    Correct linters errors.

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MessagesController < ApplicationController
   def show
-    randomIndex = rand(1..5)
-    @greeting = Greeting.find(randomIndex)
+    random_index = rand(1..5)
+    @greeting = Greeting.find(random_index)
     render json: @greeting
   end
 end
